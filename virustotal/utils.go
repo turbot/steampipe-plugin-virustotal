93cb58f64a412d1b677d49dc3dc25668171a1108
misraved <ved@turbot.com>
2022-09-07 03:24:38 +0530

    Recompile plugin with steampipe-plugin-sdk v4.1.6 and Go version 1.19

--- virustotal/utils.go
+++ virustotal/utils.go
@@ -8,8 +8,8 @@ import (
 
 	virustotal "github.com/VirusTotal/vt-go"
 
-	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/transform"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin/transform"
 )
 
 func connect(_ context.Context, d *plugin.QueryData) (*virustotal.Client, error) {
